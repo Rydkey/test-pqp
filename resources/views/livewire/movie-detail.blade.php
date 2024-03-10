c75cc13122e24dc2bd6deddeef453cd230d5d63d
Barunabe <rydkeyproduction@gmail.com>
2024-03-10 19:34:13 +0100

    feat: display movie image

--- resources/views/livewire/movie-detail.blade.php
+++ resources/views/livewire/movie-detail.blade.php
@@ -1,21 +1,27 @@
 <div class="container mx-auto p-4">
-    <div class="max-w-xl mx-auto bg-white shadow-md rounded-lg overflow-hidden">
-        <div class="bg-cover bg-center h-56 p-4" style="background-image: url('{{ $movie->backdrop_path }}')">
-            <div class="flex justify-end">
-                <span class="text-white bg-gray-800 bg-opacity-75 rounded-full px-2 py-1 text-xs font-bold">
-                    {{ $movie->vote_average * 10 }}% Rating
-                </span>
+    <div class="max-w-2xl mx-auto bg-white shadow-md rounded-lg overflow-hidden">
+        <div class="p-4 flex">
+            <div class="flex-1 w-2">
+                <img src="https://image.tmdb.org/t/p/w200{{$movie->poster_path}}" alt="Description"
+                     class="object-none object-center rounded">
             </div>
-        </div>
-        <div class="p-4">
-            <h2 class="font-bold text-3xl mb-2">{{ $movie->title }}</h2>
-            <p class="text-gray-700">{{ $movie->overview }}</p>
-            <div class="mt-4">
-                <h3 class="font-bold">Details</h3>
+            <div class="flex-1 w-4">
+                <h1 class="text-3xl font-bold mb-2">{{ $movie->title }}</h1>
+                <p class="text-gray-700 mb-4">{{ $movie->overview }}</p>
                 <ul>
-                    <li><strong>Release Date:</strong> {{ $movie->release_date }}</li>
-                    <li><strong>Popularity:</strong> {{ $movie->popularity }}</li>
-                    <li><strong>Language:</strong> {{ $movie->original_language }}</li>
+                    <li><strong>Date de sortie :</strong> {{ $movie->release_date }}</li>
+                    <!-- Remplacez 'Date' par votre donnée dynamique -->
+                    <li><strong>Genre :</strong>
+                        @foreach($movie->genres as $genre)
+                            {{$genre->name}}
+                            @if(!$loop->last)
+                                ,
+                            @endif
+                        @endforeach
+                    </li>
+                    <!-- Remplacez 'Genre' par votre donnée dynamique -->
+                    <li><strong>Langage :</strong> {{ $movie->original_language }}</li>
+                    <!-- Remplacez 'Durée' par votre donnée dynamique -->
                 </ul>
             </div>
         </div>
